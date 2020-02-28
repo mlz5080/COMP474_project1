60e3bbf6f166f7d4de7423512f0ea33069d68d82
Ling Zhi Mo <mlz961116@gmail.com>
2020-02-28 16:16:27 -0500

    first init

---
+++ con_open_api.py
@@ -0,0 +1,71 @@
+import requests,ast
+from requests_futures.sessions import FuturesSession
+from concurrent.futures import as_completed
+import json
+
+User="124"
+Key="d3c1c8924a8d59f9b5f29f54bbbbb5e7"
+#API format
+#Any parameter could be replaced by *
+#Course
+#GET course/description/filter/{courseID}
+#Catalog
+#GET course/catalog/filter/{subject}/{catalog}/{carreer}
+
+base_url="https://opendata.concordia.ca/API/v1/"
+raw_string_course=requests.get(base_url+"course/description/filter/*",auth=(User,Key)).content.decode()
+raw_string_catalog=requests.get(base_url+"course/catalog/filter/*/*/*",auth=(User,Key)).content.decode()
+raw_string_catalog=raw_string_catalog.replace("null","None")
+#print(raw_string_catalog)
+
+
+#dicionaries
+string_list_course = ast.literal_eval(raw_string_course)
+string_list_catalog = eval(raw_string_catalog)
+# print(string_list_catalog)
+# print(string_list_course)
+print(len(string_list_course),len(string_list_catalog))
+
+def writetofile():
+	with open("course.txt","w") as file:
+		for i in string_list_course:
+			i['description']=i['description'].replace("\n"," ").replace("***","").replace("~~~","").replace("*KEYB*","").replace("<b>","")
+			i['description']=i['description'].replace("\\/","").replace("\r"," ").replace("\t"," ").replace("*VID*","").replace("*CNT*","")
+			file.write(json.dumps(i))
+			file.write("\n")
+
+	with open("catalog.txt","w") as file:
+		for i in string_list_catalog:
+			i['title'] = i['title'].replace("\\/"," ").replace("\t"," ")
+			i['prerequisites'] = i['prerequisites'].replace("\\/"," ").replace("\n"," ")
+			file.write(json.dumps(i))
+			file.write("\n")
+
+course_set=set([i["ID"] for i in string_list_course])
+catalog_set=set([i["ID"] for i in string_list_catalog])
+difference = course_set-catalog_set
+
+writetofile()
+
+with open("difference.txt","w") as file:
+	for i in difference:
+		file.write(i)
+		file.write("\n")
+
+# course_set=set([i["ID"] for i in string_list_course])
+# catalog_set=set([i["ID"] for i in string_list_catalog])
+# print(len(course_set),len(catalog_set))
+
+content=[]
+list_thread=[]
+list_response=[]
+def multiple_requests():
+	with FuturesSession(max_workers=8,session=requests.Session()) as rq_session:
+		for cid in course_set-catalog_set:
+			list_thread.append(rq_session.get(base_url+"course/description/filter/"+cid,auth=(User,Key)))
+
+		for index,job in enumerate(as_completed(list_thread)):
+			list_response.append(job.result())
+
+		for response in list_response:
+			print(response.content.decode())
