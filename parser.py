f84003a2b1c4355cee3d23156fc8c4f51947567a
fjodoin <filipjodoin@hotmail.com>
2020-02-29 18:59:04 -0500

    TOPICS functional; WARNING: threaded DBpedia Spotlight API calls will block you. Use single calls ONLY

--- parser.py
+++ parser.py
@@ -2,7 +2,9 @@ import json
 from rdflib import *
 from rdflib.namespace import *
 from rdflib.util import *
-import requests, ast
+import requests
+from requests_futures.sessions import FuturesSession
+from concurrent.futures import as_completed
 
 dbp = Namespace("http://dbpedia.org/resource/")
 
@@ -15,11 +17,12 @@ nsm.bind('focu', 'http://focu.io/schema#')
 
 file = open('final_copy.txt', 'r')
 lines = file.readlines()
+file.close()
+
 iterator = 0
 for line in lines:
 		
 	temp = json.loads(line)
-
 	# COURSE
 	title_id = list(temp.keys())[0] 
 	title = temp[title_id]['title'] # Title (String)
@@ -28,6 +31,7 @@ for line in lines:
 	career = temp[title_id]['career'] # Career (String)
 	description = temp[title_id]['description'] # Description (String)
 
+	subject_catalog = from_n3('ex:' + subject + catalog, nsm=nsm)
 
 	base_url = "https://api.dbpedia-spotlight.org/en/"
 	header = {'accept': "application/json"}
@@ -35,33 +39,27 @@ for line in lines:
 	
 	dbpedia_spotlight_response = requests.get(base_url+"annotate", headers=header, params=params)
 	dbpedia_spotlight_string = dbpedia_spotlight_response.content.decode("utf-8")
+	print(dbpedia_spotlight_string)
 	dbpedia_spotlight_dict = json.loads(dbpedia_spotlight_string)
-	for key in dbpedia_spotlight_dict:
-		print (key)		
 
+	for list_item in dbpedia_spotlight_dict['Resources']:
+		for dict_item_key, dict_item_value in list_item.items():
+			if dict_item_key == '@URI':
+				uri_ref = URIRef(dict_item_value)
+				g.add((subject_catalog, FOAF.topic, uri_ref))
+				break
 
-	# print(title)
-	# print(subject)
-	# print(catalog)
-	# print(career)
-	# print(description)
-	# print(topics)
+	print("out")
 	
-	subject_catalog = from_n3('ex:' + subject + catalog, nsm=nsm)
-
 	g.add((subject_catalog, RDF.type, from_n3('dbr:Course_(education)', nsm=nsm)))
 	g.add((subject_catalog, RDFS.label, Literal(title)))
 	g.add((subject_catalog, from_n3('focu:subject', nsm=nsm), Literal(subject)))
 	g.add((subject_catalog, from_n3('focu:catalog', nsm=nsm), Literal(catalog)))
 	g.add((subject_catalog, RDFS.comment, Literal(description)))
 
-	iterator += 1
-	if (iterator == 5):
-		break
 
-
-# for s, p, o in g:
-# 	print(s, p, o)
+for s, p, o in g:
+	print(s, p, o)
 
 
 
